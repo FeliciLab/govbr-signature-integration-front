bf9c36264a8abe6cd7b815a1c804765a346cec34
Ericson Moreira <moreira.ericson@gmail.com>
2022-10-03 11:21:22 -0300

    ✨ add icons-material

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,20 +1,31 @@
-import { Button, Container, TextField } from "@mui/material";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import {
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Link,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { purple } from "@mui/material/colors";
 import fileDownload from "js-file-download";
 import React from "react";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
-import { Link, useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { useLocalStorage } from "usehooks-ts";
 import signFile from "../../resources/singFile";
 import singFileInLote from "../../resources/singFileInLote";
 import getGovBrUri, { GetGovBrUriScope } from "../../utils/getGovBrUri";
-import { Field, Options } from "./styles";
-
 interface HomeFormData {
   pdfs: File[];
 }
 
 const Home: React.FC = () => {
+  const navigate = useNavigate();
+
   const [searchParams] = useSearchParams();
 
   const code = searchParams.get("code");
@@ -51,39 +62,56 @@ const Home: React.FC = () => {
   };
 
   return (
-    <Container maxWidth="sm">
-      <h1>Assinador</h1>
-      {code ? (
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Button variant="outlined" component="label">
-            Upload do arquivo
-            <input
-              hidden
-              type="file"
-              accept="application/pdf"
-              multiple={scope === "signature_session"}
-              {...register("pdfs")}
-            />
-          </Button>
-          <Options>
-            <button type="submit">Enviar</button>
-            <Link to="/">Voltar</Link>
-          </Options>
-        </form>
-      ) : (
-        <Options>
-          <a href={getGovBrUri("sign")} onClick={() => setScope("sign")}>
-            Assinatura um arquivo
-          </a>
-          <a
-            href={getGovBrUri("signature_session")}
-            onClick={() => setScope("signature_session")}
-          >
-            Assinatura arquivo em Lote
-          </a>
-        </Options>
-      )}
-    </Container>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <Container maxWidth="sm">
+        <Typography variant="h4">Assinador</Typography>
+        {code ? (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Stack spacing={2}>
+              <Button
+                variant="outlined"
+                component="label"
+                endIcon={<CloudUploadIcon />}
+              >
+                Upload do arquivo
+                <input
+                  hidden
+                  type="file"
+                  accept="application/pdf"
+                  multiple={scope === "signature_session"}
+                  {...register("pdfs")}
+                />
+              </Button>
+              <Button variant="contained" type="submit">
+                Enviar
+              </Button>
+              <IconButton onClick={() => navigate("/")}>
+                <ChevronLeftIcon />
+              </IconButton>
+            </Stack>
+          </form>
+        ) : (
+          <Stack spacing={2}>
+            <Link href={getGovBrUri("sign")} onClick={() => setScope("sign")}>
+              Assinatura um arquivo
+            </Link>
+            <Link
+              href={getGovBrUri("signature_session")}
+              onClick={() => setScope("signature_session")}
+            >
+              Assinatura arquivo em Lote
+            </Link>
+          </Stack>
+        )}
+      </Container>
+    </Box>
   );
 };
 
