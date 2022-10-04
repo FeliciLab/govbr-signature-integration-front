2bc3c20f03d3ecafe606a48a42e7a04d3e379690
Ericson Moreira <moreira.ericson@gmail.com>
2022-10-04 10:22:56 -0300

    ✨ add .env configs intelisense

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_REDIRECT_URI: string;
+  readonly VITE_CLIENTE_ID: string;
+  readonly VITE_SECRET: string;
+  readonly VITE_SERVER_OAUTH: string;
+  readonly VITE_API_URI: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
